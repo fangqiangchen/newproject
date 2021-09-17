680b462f186ead98c800046d9e6acb645968e752
chenfangqiang <245913@qq.com>
2021-09-18 00:13:50 +0800

    python3 fire new_json 1

--- python_dash/main.py
+++ python_dash/main.py
@@ -2,7 +2,11 @@ import dash
 import dash_html_components as html
 
 app = dash.Dash(__name__)
-block = html.Div(children=["Hello",
-                           html.H1("hello again")])
+block = html.Div(
+    [
+        html.H1("Use DASH write all stuff!",
+                style={"fontSize":"6rem","textAlign":"center"}),
+        html.Img(src="/asserts/meme.png")
+                ],style={"textAlign":"center"})
 app.layout = block
 app.run_server(debug=False)
